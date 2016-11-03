c7f1817d5612b1fb3f3839c48468044770958172
bsanehi <c14558463@mydit.ie>
2016-11-03 19:37:13 +0000

    docker file

--- app/app.py
+++ app/app.py
@@ -16,6 +16,25 @@ def show_user_profile(username):
 	return "Hello user %s " % username
 
 
+@app.route('/index')
+
+def index():
+	
+	user = {'nickname': 'Cloud User'} # fake user
+
+	return '''
+
+<html>
+	<head>
+	<title>Home Page</title>
+	</head>
+
+	<body>
+	<h1>Hello, ''' + user['nickname'] + '''</h1>
+	</body>
+</html>
+'''
+
 if __name__== "__main__":
 	
-	app.run(host="0.0.0.0",port=8080, debug=True)
+	app.run(host="0.0.0.0",port=3000, debug=True)
